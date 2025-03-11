bf54d0f494a81c403300ca991cfcafb8cd039c17
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2025-03-11 23:41:05 +0530

    Implement social page and css

---
+++ src/components/home/SocialBar.tsx
@@ -0,0 +1,61 @@
+import { useRef } from "react";
+import hoverSoundFile from "../../assets/effects/pop-sound-effect-226108.mp3";
+import "./social.css";
+
+const Social = () => {
+    const hoverSound = useRef(new Audio(hoverSoundFile));
+
+    const playSound = () => {
+        if (hoverSound.current) {
+            hoverSound.current.currentTime = 0;
+            hoverSound.current.play().catch((error) => {
+                console.error("Error playing sound:", error);
+            });
+        }
+    };
+
+    return (
+        <div className="home__social">
+            <div className="group relative">
+                <a
+                    href="https://github.com/rashiniashinsana"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="home_github-icon"
+                    onMouseEnter={playSound}
+                >
+                    <i className="uil uil-github-alt"></i>
+                </a>
+                <span className="tooltip-content">GitHub</span>
+            </div>
+
+            <div className="group relative">
+                <a
+                    href="https://www.linkedin.com/in/rashini-vithanage-346b072a4/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="home__likedin-icon"
+                    onMouseEnter={playSound}
+                >
+                    <i className="uil uil-linkedin"></i>
+                </a>
+                <span className="tooltip-content">LinkedIn</span>
+            </div>
+
+            <div className="group relative">
+                <a
+                    href="https://discord.com/users/your-discord-id"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="home__discord-icon"
+                    onMouseEnter={playSound}
+                >
+                    <i className="uil uil-discord"></i>
+                </a>
+                <span className="tooltip-content">Discord</span>
+            </div>
+        </div>
+    );
+};
+
+export default Social;
