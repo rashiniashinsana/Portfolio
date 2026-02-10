27ff43f11d3799aaf7643efc3803a863de048967
Rashini Vithanage <rashinivithanage2@gmail.com>
2026-02-10 23:37:44 +0530

    feat: add premium navbar with mobile menu icons and fixed CV download

---
+++ src/components/animationCursor/AnimeBackground.tsx
@@ -0,0 +1,59 @@
+// import './index.scss';
+//
+// const AnimeBackground = () => {
+//     return (
+//         <div className="stars">
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//             <div className="star"></div>
+//         </div>
+//     )
+// }
+// export default AnimeBackground;
